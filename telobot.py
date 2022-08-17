5981a692e45a0faba00d8ca13f63b4bd6d64f098
miumiu <pavlovNP1992@yandex.ru>
2022-08-17 14:57:53 +0300

    1

--- telobot.py
+++ telobot.py
@@ -4,11 +4,25 @@ TOKEN = '5362674577:AAERla3VxzYoyApN0Dvghl_X2bux9ZthEeU'
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'hello')
 
+keys = {'биток': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        }
 
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате: \n<имя валюты> \
+    <какую валюту перевести> \
+    <количество переводимой валюты>'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты'
+    for key in keys.keys():
+        text = '\n',join((text, key,))
+    bot.reply_to(message, text)
 
 
 bot.polling()
--- Черновик.py
+++ Черновик.py
@@ -1,2 +1,5 @@
 import telebot
 help(telebot)
+@bot.message_handler()
+def echo_test(message: telebot.types.Message):
+    bot.send_message(message.chat.id, 'hello')
