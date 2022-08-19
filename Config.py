49bfa467e2b605fe32c9dba93ec4f7753995d37a
miumiu <pavlovNP1992@yandex.ru>
2022-08-19 17:43:30 +0300

    1

---
+++ Config.py
@@ -0,0 +1,6 @@
+keys = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        }
+
+TOKEN = '5362674577:AAERla3VxzYoyApN0Dvghl_X2bux9ZthEeU'
--- Черновик.py
+++
@@ -1,9 +0,0 @@
-import telebot
-help(telebot)
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'Привет раб системы, жми /start, чтобы начать')
-
-    if len(values) != 3:
-            raise ConvertionException('Эээээ, слыш, нормально введи.')
-        quote, base, amount = message.text.split('')
